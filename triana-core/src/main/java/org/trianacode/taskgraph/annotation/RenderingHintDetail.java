1b525e3ea30404a4c56a124c3e887854c84f793a
Andrew Harrison <doctor.zob@gmail.com>
2010-09-09 14:57:55 +0000

    check in

---
+++ triana-core/src/main/java/org/trianacode/taskgraph/annotation/RenderingHintDetail.java
@@ -0,0 +1,15 @@
+package org.trianacode.taskgraph.annotation;
+
+/**
+ * The hint value must match a rendering hint given in the Tool annotation
+ *
+ * @author Andrew Harrison
+ * @version 1.0.0 Aug 2, 2010
+ */
+@java.lang.annotation.Target({java.lang.annotation.ElementType.FIELD})
+@java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+public @interface RenderingHintDetail {
+
+    String hint();
+    String detail();
+}
