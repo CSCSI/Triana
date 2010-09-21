cf15df1ca56baa760b43d18ba2aabea97bf21f67
Ian Harvey <iharvey111@gmail.com>
2010-09-21 13:13:55 +0000

    JobUnitPanel

--- triana-pegasus/src/main/java/org/trianacode/pegasus/dax/JobUnitPanel.java
+++ triana-pegasus/src/main/java/org/trianacode/pegasus/dax/JobUnitPanel.java
@@ -3,9 +3,9 @@ package org.trianacode.pegasus.dax;
 import org.trianacode.gui.panels.ParameterPanel;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
@@ -17,40 +17,44 @@ import java.awt.event.ItemListener;
  * To change this template use File | Settings | File Templates.
  */
 public class JobUnitPanel extends ParameterPanel {
-    private JPanel upperPanel = new JPanel(new GridLayout(2,2,5,5));
+    private JPanel upperPanel = new JPanel(new GridLayout(3,2,5,5));
     private JPanel lowerPanel = new JPanel(new GridLayout(2,2,5,5));
     private JTextField nameField = new JTextField("");
+    private JTextField argsField = new JTextField("");
+    private int numberOfJobs;
+
     JLabel collect = new JLabel("Not a collection");
 
+    public boolean isAutoCommitByDefault(){return true;}
 
     @Override
     public void init() {
 
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        upperPanel.setBorder(javax.swing.BorderFactory.createTitledBorder("File"));
-        JLabel nameLabel = new JLabel("File Name :");
+        upperPanel.setBorder(javax.swing.BorderFactory.createTitledBorder("Job"));
+        JLabel nameLabel = new JLabel("Job Name :");
         upperPanel.add(nameLabel);
 
-        nameField.setText((String) getParameter("fileName"));
-        nameField.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent actionEvent) {
-                //             setParameter("fileName", nameField.getText());
-                apply();
-            }
-        });
+        nameField.setText((String) getParameter("jobName"));
         upperPanel.add(nameField);
+
+        JLabel argsLabel = new JLabel("Job Args :");
+        upperPanel.add(argsLabel);
+
+        argsField.setText((String) getParameter("args"));
+        upperPanel.add(argsField);
 
         final JCheckBox collection = new JCheckBox("Collection", isCollection());
         collection.addItemListener(new ItemListener() {
             public void itemStateChanged(ItemEvent ie) {
                 if (collection.isSelected()) {
                     collect.setText("Collection of jobs.");
-                    setParameter("collection", true);
+                    setParameter("collection", "true");
                     setEnabling(lowerPanel, true);
                 } else {
                     collect.setText("Not a collection");
-                    setParameter("collection", false);
+                    setParameter("collection", "false");
                     setEnabling(lowerPanel, false);
                 }
             }
@@ -60,15 +64,43 @@ public class JobUnitPanel extends ParameterPanel {
 
         add(upperPanel);
 
+        lowerPanel.setBorder(javax.swing.BorderFactory.createTitledBorder("Job Collection Options"));
+        //     JPanel lowerPanel1 = new JPanel(new GridLayout(3, 2, 5, 5));
+        final JLabel numberLabel = new JLabel("No. Jobs : 1");
+
+        final JSlider slide = new JSlider(1, 999, 1);
+        slide.setMajorTickSpacing(100);
+        slide.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent changeEvent) {
+                numberOfJobs = slide.getValue();
+                numberLabel.setText("No. jobs : " + numberOfJobs);
+            }
+        });
+        lowerPanel.add(numberLabel);
+        lowerPanel.add(slide);
         add(lowerPanel);
 
         //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    private void apply(){
-        setParameter("fileName", nameField.getText());
-        getTask().setToolName(nameField.getText());
+    private void setParams(){
+        setParameter("args", argsField.getText());
+        setParameter("numberOfJobs", numberOfJobs);
     }
+
+    public void changeToolName(String name){
+        System.out.println("Changing tool " + getTask().getToolName() + " to : " + name);
+        setParameter("jobName", name);
+        getTask().setToolName(name);
+    }
+
+    public void apply(){
+        changeToolName(nameField.getText());
+        setParams();
+    }
+
+    public void applyClicked(){ apply();}
+    public void okClicked(){ apply();};
 
     public void setEnabling(Component c,boolean enable) {
         c.setEnabled(enable);
@@ -78,9 +110,13 @@ public class JobUnitPanel extends ParameterPanel {
             for (int j=0;j<arr.length;j++) { setEnabling(arr[j],enable); }
         }
     }
-    
+
     private boolean isCollection(){
-        return (Boolean)getParameter("collection");
+        if(getParameter("collection").equals("true")){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     @Override
