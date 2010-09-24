60efe717bb038c81f636600aacda6014f873ddbc
Andrew Harrison <doctor.zob@gmail.com>
2010-09-24 16:14:52 +0000

    checkin

--- triana-app/bin/triana.sh
+++ triana-app/bin/triana.sh
@@ -6,6 +6,6 @@ if [ "$JAVA_HOME" != "" ] ; then
     JAVA="$JAVA_HOME/bin/java"
 fi
 
-$JAVA -Djava.util.logging.config.file=./logging.properties -jar triana-app-@version@.jar
+$JAVA -Djava.util.logging.config.file=./logging.properties -jar triana-app-@version@.jar $*
 
 
