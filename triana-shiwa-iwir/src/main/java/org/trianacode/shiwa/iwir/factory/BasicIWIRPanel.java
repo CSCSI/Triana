4812f33426093d38414ff5ee060800b646d94fd7
Ian Harvey <iharvey111@gmail.com>
2011-07-19 18:11:49 +0000

    iwir creation

---
+++ triana-shiwa-iwir/src/main/java/org/trianacode/shiwa/iwir/factory/BasicIWIRPanel.java
@@ -0,0 +1,54 @@
+package org.trianacode.shiwa.iwir.factory;
+
+import org.trianacode.gui.panels.ParameterPanel;
+import org.trianacode.taskgraph.Node;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BasicIWIRPanel extends ParameterPanel {
+
+    @Override
+    public void init() {
+        setLayout(new GridLayout(1, 1));
+
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new GridLayout(4, 1));
+
+        JLabel jLabel = new JLabel("Qualified name : " + getTask().getQualifiedTaskName());
+        jPanel.add(jLabel);
+
+        jPanel.add(new JLabel("Has task : " + (getTask() != null)));
+
+        JTextArea jTextArea = new JTextArea();
+        jPanel.add(jTextArea);
+        jTextArea.append(getDescription());
+
+        this.add(jPanel);
+    }
+
+    private String getDescription() {
+        String inputNodeString = "";
+        for (Node node : getTask().getInputNodes()) {
+            inputNodeString += "\n" + node.getName();
+        }
+        String outputNodeString = "";
+        for (Node node : getTask().getOutputNodes()) {
+            outputNodeString += "\n" + node.getName();
+        }
+        String string = "\nTask " + getTask().getDisplayName() +
+                "\n     Input nodes : " + inputNodeString +
+                "\n     Output nodes : " + outputNodeString;
+        return string;
+    }
+
+    @Override
+    public void reset() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void dispose() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ triana-shiwa-iwir/src/main/java/org/trianacode/shiwa/iwir/factory/TaskHolderFactory.java
@@ -0,0 +1,53 @@
+package org.trianacode.shiwa.iwir.factory;
+
+import org.shiwa.fgi.iwir.*;
+import org.trianacode.shiwa.iwir.holders.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ian
+ * Date: 24/06/2011
+ * Time: 15:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class TaskHolderFactory {
+
+    private static TaskHolderFactory taskHolderFactory = new TaskHolderFactory();
+
+    private TaskHolderFactory() {
+    }
+
+    public static TaskHolderFactory getTaskHolderFactory() {
+        return taskHolderFactory;
+    }
+
+    public TaskHolder getTaskHolder(AbstractTask iwirTask) {
+        TaskHolder taskHolder = null;
+        if (iwirTask instanceof ForEachTask) {
+            taskHolder = new ForEachTaskHolder();
+        }
+        if (iwirTask instanceof ForTask) {
+            taskHolder = new ForTaskHolder();
+        }
+        if (iwirTask instanceof IfTask) {
+            taskHolder = new IfTaskHolder();
+        }
+        if (iwirTask instanceof ParallelForEachTask) {
+            taskHolder = new ParallelForEachTaskHolder();
+        }
+        if (iwirTask instanceof ParallelForTask) {
+            taskHolder = new ParallelForTaskHolder();
+        }
+        if (iwirTask instanceof WhileTask) {
+            taskHolder = new WhileTaskHolder();
+        }
+        if (taskHolder == null) {
+            taskHolder = new AtomicTaskHolder();
+        }
+        taskHolder.setIWIRTask(iwirTask);
+        taskHolder.registerIWIRTask(iwirTask);
+        return taskHolder;
+    }
+
+
+}
