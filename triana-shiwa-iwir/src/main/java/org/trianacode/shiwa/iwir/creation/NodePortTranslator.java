179282e5e397dd1c5053788733893c12f22d141f
Ian Harvey <iharvey111@gmail.com>
2011-07-22 15:12:21 +0000

    iwir read write

--- triana-shiwa-iwir/src/main/java/org/trianacode/shiwa/iwir/creation/NodePortTranslator.java
+++ triana-shiwa-iwir/src/main/java/org/trianacode/shiwa/iwir/creation/NodePortTranslator.java
@@ -16,46 +16,49 @@ import java.util.Vector;
  */
 public class NodePortTranslator {
 
-    HashMap<AbstractPort, Node> portNodeHashMap;
-    HashMap<Node, AbstractPort> nodeAbstractPortHashMap;
+    HashMap<String, Node> portNodeHashMap;
+    HashMap<Node, String> nodeAbstractPortIDHashMap;
+    HashMap<String, AbstractPort> abstractPortHashMap;
     Vector<NodeProxy> nodeProxies;
 
     public NodePortTranslator() {
-        portNodeHashMap = new HashMap<AbstractPort, Node>();
-        nodeAbstractPortHashMap = new HashMap<Node, AbstractPort>();
+        portNodeHashMap = new HashMap<String, Node>();
+        nodeAbstractPortIDHashMap = new HashMap<Node, String>();
+        abstractPortHashMap = new HashMap<String, AbstractPort>();
         nodeProxies = new Vector<NodeProxy>();
     }
 
     public void addNodeProxy(NodeProxy nodeProxy) {
-        portNodeHashMap.put(nodeProxy.getAbstractPort(), nodeProxy.getNode());
-        nodeAbstractPortHashMap.put(nodeProxy.getNode(), nodeProxy.getAbstractPort());
+        portNodeHashMap.put(nodeProxy.getAbstractPort().getUniqueId(), nodeProxy.getNode());
+        nodeAbstractPortIDHashMap.put(nodeProxy.getNode(), nodeProxy.getAbstractPort().getUniqueId());
+        abstractPortHashMap.put(nodeProxy.getAbstractPort().getUniqueId(), nodeProxy.getAbstractPort());
         nodeProxies.add(nodeProxy);
 
-        System.out.println("\n\n\nRecording  abstract : " + nodeProxy.getAbstractPort().getUniqueId() + " node : " + nodeProxy.getNode().getName());
+        System.out.println("\n\nRecording  abstract : " + nodeProxy.getAbstractPort().getUniqueId() + " node : " + nodeProxy.getNode().getName());
 
-        listAll();
+//        listAll();
     }
 
-    public Node getNodeForAbstractPort(AbstractPort abstractPort) {
-        return portNodeHashMap.get(abstractPort);
+    public Node getNodeForAbstractPort(String abstractPortID) {
+        return portNodeHashMap.get(abstractPortID);
     }
 
     public AbstractPort getAbstractPortForNode(Node node) {
-        return nodeAbstractPortHashMap.get(node);
+        return abstractPortHashMap.get(nodeAbstractPortIDHashMap.get(node));
     }
 
     public void listAll() {
         System.out.println("\n*** Listing all node stuff, abstract key");
         for (Map.Entry entry : portNodeHashMap.entrySet()) {
-            AbstractPort abstractPort = (AbstractPort) entry.getKey();
+            String abstractPortID = (String) entry.getKey();
             Node node = (Node) entry.getValue();
-            System.out.println(abstractPort.getUniqueId() + " " + node.getName());
+            System.out.println(abstractPortID + " " + node.getName());
         }
         System.out.println("\n***Node key");
-        for (Map.Entry entry : nodeAbstractPortHashMap.entrySet()) {
-            AbstractPort abstractPort = (AbstractPort) entry.getValue();
+        for (Map.Entry entry : nodeAbstractPortIDHashMap.entrySet()) {
+            String abstractPortID = (String) entry.getValue();
             Node node = (Node) entry.getKey();
-            System.out.println(node.getName() + " " + abstractPort.getUniqueId());
+            System.out.println(node.getName() + " " + abstractPortID);
         }
     }
 }
