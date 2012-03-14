1156b7c63bd8de1f271107c344ae1a64aa4574b0
Ian Harvey <iharvey111@gmail.com>
2012-03-14 18:35:34 +0000

    run bundle in pool

--- triana-shiwa/src/main/java/org/trianacode/shiwa/bundle/TrianaBundle.java
+++ triana-shiwa/src/main/java/org/trianacode/shiwa/bundle/TrianaBundle.java
@@ -3,9 +3,11 @@ package org.trianacode.shiwa.bundle;
 import org.shiwa.desktop.data.description.SHIWABundle;
 import org.shiwa.desktop.data.description.bundle.BundleFile;
 import org.shiwa.desktop.data.description.core.Configuration;
+import org.shiwa.desktop.data.description.resource.ConfigurationResource;
+import org.shiwa.desktop.data.description.resource.ReferableResource;
+import org.shiwa.desktop.data.description.workflow.OutputPort;
 import org.shiwa.desktop.data.transfer.WorkflowController;
 import org.shiwa.desktop.data.util.DataUtils;
-import org.shiwa.desktop.data.util.exception.SHIWADesktopIOException;
 import org.trianacode.TrianaInstance;
 import org.trianacode.enactment.Exec;
 import org.trianacode.enactment.addon.CLIaddon;
@@ -18,6 +20,9 @@ import org.trianacode.taskgraph.tool.Tool;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,80 +35,125 @@ public class TrianaBundle {
 
     public byte[] executeBundle(SHIWABundle shiwaBundle, String[] trianaArgs) {
 
-        TrianaInstance engine = null;
+        WorkflowController workflowController = null;
         try {
-            engine = new TrianaInstance(trianaArgs);
-            engine.addExtensionClass(CLIaddon.class);
-            engine.init();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            workflowController = new WorkflowController(shiwaBundle);
+            for (Configuration configuration : workflowController.getConfigurations()) {
+                if (configuration.getType() == Configuration.ConfigType.ENVIRONMENT_CONFIGURATION) {
+                    for (ConfigurationResource res : configuration.getResources()) {
+//                        res.getId()
+//                        workflowController.getWorkflowImplementation().getDependency()
+//                        res.getBundleFile().getBytes();
+//                        res.getReferableResource().
+                    }
+                }
+            }
 
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
-        Unbundler unbundler = new Unbundler();
-        Tool tool = null;
-        String dataLocation = null;
-        try {
-            tool = unbundler.getTool(engine, shiwaBundle);
-            dataLocation = unbundler.getConfigFile().getAbsolutePath();
-            unbundler.serializeOutputs((TaskGraph) tool);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (TaskGraphException e) {
-            e.printStackTrace();
-        } catch (ProxyInstantiationException e) {
-            e.printStackTrace();
-        }
+            TrianaInstance engine = null;
+            try {
+                engine = new TrianaInstance(trianaArgs);
+                engine.addExtensionClass(CLIaddon.class);
+                engine.init();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
-        Exec exec = new Exec(null);
-        try {
-            exec.execute(tool, dataLocation);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            Unbundler unbundler = new Unbundler();
+            File homeFolder = new File(System.getProperty("user.home"));
+            File runFolder = null;
+            if (homeFolder.exists()) {
+                runFolder = new File(homeFolder, getTimeStamp());
+                runFolder.mkdirs();
+            }
+            unbundler.setRuntimeOutputFolder(runFolder);
+            Tool tool = null;
+            String dataLocation = null;
+            try {
+                tool = unbundler.getTool(engine, shiwaBundle);
+                dataLocation = unbundler.getConfigFile().getAbsolutePath();
+                unbundler.serializeOutputs((TaskGraph) tool);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (TaskGraphException e) {
+                e.printStackTrace();
+            } catch (ProxyInstantiationException e) {
+                e.printStackTrace();
+            }
 
-        if (engine != null) {
-            engine.shutdown(0);
-        }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
-        try {
-            WorkflowController workflowController = new WorkflowController(shiwaBundle);
+            Exec exec = new Exec(null);
+            try {
+                exec.execute(tool, dataLocation);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            if (engine != null) {
+                engine.shutdown(-1);
+            }
+
             Configuration execConfig = new Configuration(Configuration.ConfigType.EXECUTION_CONFIGURATION);
-            for (File outputFile : unbundler.getOutputFiles()) {
+            ArrayList<ConfigurationResource> configurationResourceArrayList = new ArrayList<ConfigurationResource>();
+
+            ArrayList<File> outputs = unbundler.getOutputFiles();
+            ArrayList<OutputPort> outputPorts = new ArrayList<OutputPort>();
+
+            for (ReferableResource resource : workflowController.getWorkflowImplementation().getSignature().getPorts()) {
+                if (resource instanceof OutputPort) {
+                    outputPorts.add((OutputPort) resource);
+                }
+            }
+
+            for (int i = 0; i < outputs.size(); i++) {
+                //TODO check name
+                ConfigurationResource configurationResource = new ConfigurationResource(outputPorts.get(i));
+                File outputFile = outputs.get(i);
+                System.out.println(outputFile.getAbsolutePath());
                 BundleFile bf = DataUtils.createBundleFile(outputFile, execConfig.getId() + "/");
                 bf.setType(BundleFile.FileType.INPUT_FILE);
                 execConfig.getBundleFiles().add(bf);
+                configurationResource.setBundleFile(bf);
+                configurationResource.setRefType(ConfigurationResource.RefTypes.FILE_REF);
+                execConfig.addResourceRef(configurationResource);
+
+                configurationResourceArrayList.add(configurationResource);
             }
 
-            workflowController.getWorkflowImplementation().getAggregatedResources().add(execConfig);
-        } catch (SHIWADesktopIOException e) {
-            e.printStackTrace();
-        }
+            execConfig.setResources(configurationResourceArrayList);
 
-        try {
-            File bundleFile = DataUtils.bundle(File.createTempFile("bundle", ".tmp"), shiwaBundle.getAggregatedResource());
+            workflowController.getWorkflowImplementation().getAggregatedResources().add(execConfig);
+
+
+            File tempBundle = File.createTempFile("bundle", ".tmp");
+            System.out.println("Output bundle : " + tempBundle.getAbsolutePath());
+            File bundleFile = DataUtils.bundle(tempBundle, shiwaBundle.getAggregatedResource());
             return getFileBytes(bundleFile);
-        } catch (IOException e) {
-            e.printStackTrace();
         } catch (Exception e) {
+            System.out.println(e.getMessage());
             e.printStackTrace();
         }
 
         return null;
     }
 
-    private byte[] getFileBytes(File outfile) throws Exception {
+    private static String getTimeStamp() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yy-MM-dd_HH-mm-ss-SS_z");
+        return dateFormat.format(new Date());
+    }
+
+    public static byte[] getFileBytes(File outfile) throws Exception {
         FileInputStream fis = new FileInputStream(outfile);
         long length = outfile.length();
 
@@ -142,7 +192,7 @@ public class TrianaBundle {
         if (args.length > 0) {
             filePath = args[0];
         } else {
-            filePath = "/Users/ian/stuff/concatBundle.bundle.zip";
+            filePath = "/Users/ian/stuff/testBundle.zip";
         }
 
         File file = new File(filePath);
@@ -150,7 +200,9 @@ public class TrianaBundle {
         if (file.exists()) {
             SHIWABundle shiwaBundle = new SHIWABundle(file);
 
-            new TrianaBundle().executeBundle(shiwaBundle, new String[]{});
+            byte[] bundleBytes = new TrianaBundle().executeBundle(shiwaBundle, new String[]{});
+
+            System.out.println(bundleBytes.length + " sized bundle returned.");
         } else {
             System.out.println("File not found");
         }
